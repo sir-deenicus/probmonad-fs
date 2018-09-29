b74b5d26f44c3734f1607cbd97308b6f79734f4c
Deen Abiola <sir.deenicus@gmail.com>
2018-09-30 00:47:17 +0100

    rework DiffEvolution concept

--- ProbMonadFs/MetropolisHastingsDiffEv.fs
+++ ProbMonadFs/MetropolisHastingsDiffEv.fs
@@ -1,6 +1,6 @@
 ﻿module MetropalisHastings
 
-open FSharp.Probmonad
+open Probmonad
 open ProbCSharp     
 
 module SimpleMCMC =
@@ -41,16 +41,18 @@ module DiffEvolution =
         return! (if accept then candidate else chainState)
       }
 
-  let inline adjust jitter temperature (next:ItemProb<_>) (next2:ItemProb<_>) (next3:ItemProb<_>) =
-      let diff = next2.Item - next3.Item     
-      ItemProb(next.Item + temperature * diff + jitter,next.Prob)
+  let inline adjust sim jitter temperature (next:ItemProb<_>) (next2:ItemProb<_>) (next3:ItemProb<_>) =
+      let diff = next2.Item - next3.Item   
+      let next' = next.Item + temperature * diff + jitter
+      ItemProb(next',prob ((max 1. (sim next next')) * next.Prob.Value)) //this step is problematic. Naively using the probability of the original point
+                                                                         //is unjustified. Hack: scale probability value by similarity/distance.
 
-  let inline iterate2 jitter temperature n proposal (chain:list<ItemProb<_>>) chain2 chain3 =
+  let inline iterate sim jitter atten t n proposal (chain:list<ItemProb<_>>) chain2 chain3 =
       if (n <= 0) then new Pure<_>(chain)
       else            
 
-      let rec loop newChain chainState chainState2 chainState3 n =
-          if n = 0 then newChain
+      let rec loop temperature currentChain chainState chainState2 chainState3 n =
+          if n = 0 then currentChain
           else
           let nextDist = sampleChain proposal chainState 
           let nextDist2 = sampleChain proposal chainState2      
@@ -60,16 +62,16 @@ module DiffEvolution =
           let next2 = nextDist2.Sample();
           let next3 = nextDist3.Sample();
 
-          let next' = adjust jitter temperature next next2 next3
-          let next2' = adjust jitter temperature next2 next next3
-          let next3' = adjust jitter temperature next3 next next2
+          let next' = adjust sim jitter temperature next next2 next3
+          let next2' = adjust sim jitter temperature next2 next next3
+          let next3' = adjust sim jitter temperature next3 next next2
                  
-          loop (next'::newChain) next' next2' next3' (n-1)
+          loop (temperature * atten) (next'::currentChain) next' next2' next3' (n-1)
             
-      Pure(loop chain (List.head chain) chain2 chain3 n)
+      Pure(loop t chain (List.head chain) chain2 chain3 n)
 
-  let inline MHPrior2 jitter temperature (dist:'a Dist) n =
+  let inline MHPrior sim jitter temperature attenuate (dist:'a Dist) n =
       let initial = [dist.WeightedPrior().Sample()]
     
-      let chain = iterate2 jitter temperature n (fun () -> dist.WeightedPrior()) initial (dist.WeightedPrior().Sample()) (dist.WeightedPrior().Sample())
+      let chain = iterate sim jitter attenuate temperature n (fun () -> dist.WeightedPrior()) initial (dist.WeightedPrior().Sample()) (dist.WeightedPrior().Sample())
       chain.Select (fun iplist -> iplist |> List.map (fun i -> i.Item))
--- ProbMonadFs/probmonad.fs
+++ ProbMonadFs/probmonad.fs
@@ -1,4 +1,4 @@
-﻿module FSharp.Probmonad
+﻿module Probmonad
                
 open Prelude.Math
 open Prelude.Common
