67dd76db9635e9164429f2af35992b0cbfc52a11
Deen Abiola <sir.deenicus@gmail.com>
2018-09-29 23:25:02 +0100

    minor refactoring

--- ProbMonadFs/MetropolisHastingsDiffEv.fs
+++ ProbMonadFs/MetropolisHastingsDiffEv.fs
@@ -1,74 +1,75 @@
-﻿module MetropalisHastingsDiffEv
+﻿module MetropalisHastings
 
 open FSharp.Probmonad
 open ProbCSharp     
 
-let iterate n proposal (chain:list<ItemProb<_>>) =
-    if (n <= 0) then new Pure<_>(chain)
-    else            
+module SimpleMCMC =
+  let iterate n proposal (chain:list<ItemProb<_>>) =
+      if (n <= 0) then new Pure<_>(chain)
+      else            
 
-    let rec loop newChain (chainState:ItemProb<_>) n =
-        if n = 0 then newChain
-        else
-        let nextDist = 
-            dist { 
-              let! candidate = proposal()
-              let p = min 1. ((candidate:ItemProb<_>).Prob.Div(chainState.Prob).Value)
+      let rec loop newChain (chainState:ItemProb<_>) n =
+          if n = 0 then newChain
+          else
+          let nextDist = 
+              dist { 
+                let! candidate = proposal()
+                let p = min 1. ((candidate:ItemProb<_>).Prob.Div(chainState.Prob).Value)
 
-              let! accept = Primitive( (bernoulliF p).ToSampleDist() )
-              return! (if accept then candidate else chainState)
-            }
-        let next = nextDist.Sample();
+                let! accept = Primitive( (bernoulliF p).ToSampleDist() )
+                return! (if accept then candidate else chainState)
+              }
+          let next = nextDist.Sample();
                  
-        loop (next::newChain) next (n-1)
+          loop (next::newChain) next (n-1)
             
-    Pure(loop chain (List.head chain) n)
+      Pure(loop chain (List.head chain) n)
 
 
-let MHPrior (dist:'a Dist) n =
-    let initial = [dist.WeightedPrior().Sample()]
-    let chain = iterate n (fun () -> dist.WeightedPrior()) initial
-    chain.Select (fun iplist -> iplist |> List.map (fun i -> i.Item))
+  let MHPrior (dist:'a Dist) n =
+      let initial = [dist.WeightedPrior().Sample()]
+      let chain = iterate n (fun () -> dist.WeightedPrior()) initial
+      chain.Select (fun iplist -> iplist |> List.map (fun i -> i.Item))
 
 /////////////////////////
+module DiffEvolution = 
+  let sampleChain proposal (chainState:ItemProb<_>) = dist { 
+        let! candidate = proposal()
+        let p = min 1. ((candidate:ItemProb<_>).Prob.Div(chainState.Prob).Value)
 
-let sampleChain proposal (chainState:ItemProb<_>) = dist { 
-      let! candidate = proposal()
-      let p = min 1. ((candidate:ItemProb<_>).Prob.Div(chainState.Prob).Value)
+        let! accept = Primitive( (bernoulliF p).ToSampleDist() )
+        return! (if accept then candidate else chainState)
+      }
 
-      let! accept = Primitive( (bernoulliF p).ToSampleDist() )
-      return! (if accept then candidate else chainState)
-    }
+  let inline adjust jitter temperature (next:ItemProb<_>) (next2:ItemProb<_>) (next3:ItemProb<_>) =
+      let diff = next2.Item - next3.Item     
+      ItemProb(next.Item + temperature * diff + jitter,next.Prob)
 
-let inline adjust jitter temperature (next:ItemProb<_>) (next2:ItemProb<_>) (next3:ItemProb<_>) =
-    let diff = next2.Item - next3.Item     
-    ItemProb(next.Item + temperature * diff + jitter,next.Prob)
+  let inline iterate2 jitter temperature n proposal (chain:list<ItemProb<_>>) chain2 chain3 =
+      if (n <= 0) then new Pure<_>(chain)
+      else            
 
-let inline iterate2 jitter temperature n proposal (chain:list<ItemProb<_>>) chain2 chain3 =
-    if (n <= 0) then new Pure<_>(chain)
-    else            
+      let rec loop newChain chainState chainState2 chainState3 n =
+          if n = 0 then newChain
+          else
+          let nextDist = sampleChain proposal chainState 
+          let nextDist2 = sampleChain proposal chainState2      
+          let nextDist3 = sampleChain proposal chainState3
 
-    let rec loop newChain chainState chainState2 chainState3 n =
-        if n = 0 then newChain
-        else
-        let nextDist = sampleChain proposal chainState 
-        let nextDist2 = sampleChain proposal chainState2      
-        let nextDist3 = sampleChain proposal chainState3
+          let next = nextDist.Sample();
+          let next2 = nextDist2.Sample();
+          let next3 = nextDist3.Sample();
 
-        let next = nextDist.Sample();
-        let next2 = nextDist2.Sample();
-        let next3 = nextDist3.Sample();
-
-        let next' = adjust jitter temperature next next2 next3
-        let next2' = adjust jitter temperature next2 next next3
-        let next3' = adjust jitter temperature next3 next next2
+          let next' = adjust jitter temperature next next2 next3
+          let next2' = adjust jitter temperature next2 next next3
+          let next3' = adjust jitter temperature next3 next next2
                  
-        loop (next'::newChain) next' next2' next3' (n-1)
+          loop (next'::newChain) next' next2' next3' (n-1)
             
-    Pure(loop chain (List.head chain) chain2 chain3 n)
+      Pure(loop chain (List.head chain) chain2 chain3 n)
 
-let inline MHPrior2 jitter temperature (dist:'a Dist) n =
-    let initial = [dist.WeightedPrior().Sample()]
+  let inline MHPrior2 jitter temperature (dist:'a Dist) n =
+      let initial = [dist.WeightedPrior().Sample()]
     
-    let chain = iterate2 jitter temperature n (fun () -> dist.WeightedPrior()) initial (dist.WeightedPrior().Sample()) (dist.WeightedPrior().Sample())
-    chain.Select (fun iplist -> iplist |> List.map (fun i -> i.Item))
+      let chain = iterate2 jitter temperature n (fun () -> dist.WeightedPrior()) initial (dist.WeightedPrior().Sample()) (dist.WeightedPrior().Sample())
+      chain.Select (fun iplist -> iplist |> List.map (fun i -> i.Item))
--- ProbMonadFs/probmonad.fs
+++ ProbMonadFs/probmonad.fs
@@ -103,215 +103,211 @@ let fdist = FDistBuilder()
 
 let dist = DistBuilder()
 
-let prob2pair (p:ItemProb<_>)= p.Item, p.Prob.Value
-                
-let rec computePosterior likelihood ps (p:Dist<_>) = function 
-     | [] -> ps 
-     | x::xs -> 
-        let p' = p.Condition(fun theta -> likelihood theta x)
-        
-        computePosterior likelihood (p'::ps) p' xs  
+let rec computePosterior likelihood (prior:Dist<_>) = function 
+     | [] -> prior 
+     | x::xs -> computePosterior likelihood (prior.Condition(fun theta -> likelihood theta x)) xs  
 
 ///////////////////    
-
-let samplesToArray data = 
-    data |> Seq.toArray 
-         |> Array.mapFilter prob2pair (fun (_,p) -> not(p = 0.|| Double.IsNaN p || Double.IsInfinity p)) 
-         |> Array.normalizeWeights
+module Grouping =
+  let prob2pair (p:ItemProb<_>)= p.Item, p.Prob.Value
+                
+  let samplesToArray data = 
+      data |> Seq.toArray 
+           |> Array.mapFilter prob2pair (fun (_,p) -> not(p = 0.|| Double.IsNaN p || Double.IsInfinity p)) 
+           |> Array.normalizeWeights
  
-let getImportanceSamples n (p:Dist<_>) = samplesToArray(p.ImportanceSamples(n).Sample())
+  let getImportanceSamples n (p:Dist<_>) = samplesToArray(p.ImportanceSamples(n).Sample())
 
 
-let roundAndGroupSamples r samples =
-     samples 
-     |> Seq.toArray 
-     |> Array.map  (round r) 
-     |> Array.groupBy id 
-     |> Array.map (keepLeft (Array.length >> float))
-     |> Array.normalizeWeights  
+  let roundAndGroupSamples r samples =
+       samples 
+       |> Seq.toArray 
+       |> Array.map  (round r) 
+       |> Array.groupBy id 
+       |> Array.map (keepLeft (Array.length >> float))
+       |> Array.normalizeWeights  
      
 
-let roundAndGroupSamplesWith f  samples =
-     samples 
-     |> Seq.toArray 
-     |> Array.map f
-     |> Array.groupBy id 
-     |> Array.map (keepLeft (Array.length >> float))
-     |> Array.normalizeWeights           
+  let roundAndGroupSamplesWith f  samples =
+       samples 
+       |> Seq.toArray 
+       |> Array.map f
+       |> Array.groupBy id 
+       |> Array.map (keepLeft (Array.length >> float))
+       |> Array.normalizeWeights           
 
 
-let compactSamples nSamples = 
-    nSamples                                     
-    |> Seq.concat 
-    |> Seq.toArray
+  let compactSamples nSamples = 
+      nSamples                                     
+      |> Seq.concat 
+      |> Seq.toArray
 
-let compactMapSamples f samples =
-    Array.map (fun (x,p:float) -> f x,p) samples
-    |> Array.groupBy fst 
-    |> Array.map (fun (x,xs) -> x, Array.sumBy snd xs) 
-    |> Array.normalizeWeights  
-
-
-let compactFiniteSamples (d:FiniteDist<_>) = 
-    d.Explicit.Weights 
-    |> mapFilter prob2pair (snd >> (<>) 0.) 
-    |> Seq.toArray  
-
-//////////////////////////
-    
-let filterWith f data = 
-  let matches = data |> Array.filter f
-  (Array.length matches |> float) / (float data.Length) 
-                       
-
-let filterWithCondition conditional f data = 
-  let sub = Array.filter conditional data
-  let matches = sub |> Array.filter f 
-  (Array.length matches |> float) / (float sub.Length) 
+  let compactMapSamples f samples =
+      Array.map (fun (x,p:float) -> f x,p) samples
+      |> Array.groupBy fst 
+      |> Array.map (fun (x,xs) -> x, Array.sumBy snd xs) 
+      |> Array.normalizeWeights  
 
 
-let inline probabilityOf filter m = 
-    Array.sumBy snd (Array.filter (fun (k, _) -> filter k) m)
+  let compactFiniteSamples (d:FiniteDist<_>) = 
+      d.Explicit.Weights 
+      |> mapFilter prob2pair (snd >> (<>) 0.) 
+      |> Seq.toArray  
 
-
-let inline conditionalProbability conditional matchwith m = 
-    let sub = Array.filter (fun (k, _) -> conditional k) m
-    let matches = Array.filter (fun (k, _) -> matchwith k) sub
-    (Array.sumBy snd matches) / (Array.sumBy snd sub)
-
-let toBits x = x / log 2. 
-
-let inline log0 x = if x = 0. then 0. else log x
-
-let inline entropy dist = -Seq.sumBy (fun (_,p) -> p * log0 p) dist
 
 ///////////////////////////////////
 
-let smoothDistr alpha data = 
-    data |> Array.fold (fun (p_prior,ps) (x,p) -> 
-                  let p' = exponentialSmoothing id alpha p_prior p
-                  p', (x,p')::ps) (snd data.[0],[])
-         |> snd
-         |> List.toArray
-         |> Array.normalizeWeights
 
 ///////////////////////
 
+module SampleSummarize = 
+  let smoothDistr alpha data = 
+      data |> Array.fold (fun (p_prior,ps) (x,p) -> 
+                    let p' = exponentialSmoothing id alpha p_prior p
+                    p', (x,p')::ps) (snd data.[0],[])
+           |> snd
+           |> List.toArray
+           |> Array.normalizeWeights
 
-let inline computeDistrAverage f d = Array.sumBy (fun (x,p) -> f x * p) d
+  let inline computeDistrAverage f d = Array.sumBy (fun (x,p) -> f x * p) d
 
-
-let computeCredible f roundTo p1 p2 dat =         
-    let rec loop cumulativeProb ps = function
-            | []  -> ps, cumulativeProb
-            | _ when cumulativeProb > p2 -> ps, cumulativeProb
-            | (x,p)::dat -> let ps' = if cumulativeProb + p < p1 then ps else ((x,p,round roundTo (p+cumulativeProb))::ps)
-                            loop (cumulativeProb+p) ps' dat
+  let computeCredible f roundTo p1 p2 dat =         
+      let rec loop cumulativeProb ps = function
+              | []  -> ps, cumulativeProb
+              | _ when cumulativeProb > p2 -> ps, cumulativeProb
+              | (x,p)::dat -> let ps' = if cumulativeProb + p < p1 then ps else ((x,p,round roundTo (p+cumulativeProb))::ps)
+                              loop (cumulativeProb+p) ps' dat
     
-    let cump, _ = loop 0. [] (List.ofSeq dat |> List.sortBy f)
+      let cump, _ = loop 0. [] (List.ofSeq dat |> List.sortBy f)
     
-    cump,  List.minBy third cump |> fst3, List.maxBy third cump |> fst3
+      cump,  List.minBy third cump |> fst3, List.maxBy third cump |> fst3
 
 
-let getLargeProbItems maxp data =
-    let rec innerloop curritems cumulativeprob = function
-          | [] -> curritems
-          | _ when cumulativeprob > maxp -> curritems
-          | ((_,p) as item::ps) -> innerloop (item::curritems) (p + cumulativeprob) ps
-    innerloop [] 0. data
+  let getLargeProbItems maxp data =
+      let rec innerloop curritems cumulativeprob = function
+            | [] -> curritems
+            | _ when cumulativeprob > maxp -> curritems
+            | ((_,p) as item::ps) -> innerloop (item::curritems) (p + cumulativeprob) ps
+      innerloop [] 0. data
 
 
-let findTopItem (vc:_[]) =
-    let topindex,_ = vc |> Array.indexed 
-                        |> Array.maxBy (snd >> snd)
-    let (_,p) as topitem = vc.[topindex]
-    topindex, p , [topitem]
+  let findTopItem (vc:_[]) =
+      let topindex,_ = vc |> Array.indexed 
+                          |> Array.maxBy (snd >> snd)
+      let (_,p) as topitem = vc.[topindex]
+      topindex, p , [topitem]
     
 
-let getBulk (minp:float) items = 
-    let rec loopinner cmin cmax bulkMass sum =
-        if sum > minp || (cmin < 0 && cmax >= Array.length items) then sum, bulkMass 
+  let getBulk (minp:float) items = 
+      let rec loopinner cmin cmax bulkMass sum =
+          if sum > minp || (cmin < 0 && cmax >= Array.length items) then sum, bulkMass 
 
-        else  let bulkMass' = let frontpart = if cmin < 0 then bulkMass else items.[cmin]::bulkMass
-                              if cmax > items.Length - 1 then frontpart else items.[cmax]::frontpart
-              let currentSum = List.sumBy snd bulkMass'                   
+          else  let bulkMass' = let frontpart = if cmin < 0 then bulkMass else items.[cmin]::bulkMass
+                                if cmax > items.Length - 1 then frontpart else items.[cmax]::frontpart
+                let currentSum = List.sumBy snd bulkMass'                   
               
-              loopinner (cmin-1) (cmax+1) bulkMass' currentSum 
+                loopinner (cmin-1) (cmax+1) bulkMass' currentSum 
 
-    let topindex,p,root = findTopItem items      
-    loopinner (topindex-1) (topindex+1) root p
+      let topindex,p,root = findTopItem items      
+      loopinner (topindex-1) (topindex+1) root p
 
 
-let getBulkAlternating (minp:float) toggle items = 
-    let rec loopinner toggle cmin cmax bulkMass sum =
-        if sum > minp || (cmin < 0 && cmax >= Array.length items) then sum, bulkMass 
-        else let cmin',cmax',bulkMass' = 
-                 match toggle with 
-                 | true -> cmin, cmax+1, if cmax > items.Length - 1 then bulkMass else items.[cmax]::bulkMass 
-                 | false -> cmin-1, cmax, if cmin >= 0 then items.[cmin]::bulkMass else bulkMass
+  let getBulkAlternating (minp:float) toggle items = 
+      let rec loopinner toggle cmin cmax bulkMass sum =
+          if sum > minp || (cmin < 0 && cmax >= Array.length items) then sum, bulkMass 
+          else let cmin',cmax',bulkMass' = 
+                   match toggle with 
+                   | true -> cmin, cmax+1, if cmax > items.Length - 1 then bulkMass else items.[cmax]::bulkMass 
+                   | false -> cmin-1, cmax, if cmin >= 0 then items.[cmin]::bulkMass else bulkMass
                
-             let currentSum = List.sumBy snd bulkMass'                   
+               let currentSum = List.sumBy snd bulkMass'                   
 
-             loopinner (not toggle) cmin' cmax' bulkMass' currentSum  
+               loopinner (not toggle) cmin' cmax' bulkMass' currentSum  
     
-    let topindex,p,root = findTopItem items
-    loopinner toggle (topindex-1) (topindex+1) root p
+      let topindex,p,root = findTopItem items
+      loopinner toggle (topindex-1) (topindex+1) root p
 
 ////////////////////////////////
-    
+module Sampling =     
+  let computeSamples nIters nPoints data = 
+      let updaterate = max 1 (nIters / 10)
+      let mutable nelements = 0
 
-let computeSamples nIters nPoints data = 
-    let updaterate = max 1 (nIters / 10)
-    let mutable nelements = 0
-
-    [|for i in 0..nIters do                          
-          let mh = MetropolisHastings.MHPrior(data, nPoints)        
-          let samples = mh.SampleN(nPoints);
-          let dat = Seq.takeOrMax nPoints samples |> Seq.toArray |> compactSamples
+      [|for i in 0..nIters do                          
+            let mh = MetropolisHastings.MHPrior(data, nPoints)        
+            let samples = mh.SampleN(nPoints);
+            let dat = Seq.takeOrMax nPoints samples |> Seq.toArray |> Grouping.compactSamples
         
-          let els = Array.countElements dat
-          nelements <- nelements + els.Count
+            let els = Array.countElements dat
+            nelements <- nelements + els.Count
         
-          if i % updaterate = 0 then 
-              printfn "%d of %d" i nIters
-              printfn "%d elements" nelements       
-          yield (els)|]
+            if i % updaterate = 0 then 
+                printfn "%d of %d" i nIters
+                printfn "%d elements" nelements       
+            yield (els)|]
 
-    |> Array.fold (fun fm m -> Map.merge (+) id m fm) Map.empty 
-    |> Map.toArray 
+      |> Array.fold (fun fm m -> Map.merge (+) id m fm) Map.empty 
+      |> Map.toArray 
 
 
-let smcSample nSamples nParticles (dist:Dist<_>) =
-    let samples = dist.SmcMultiple(nSamples, nParticles).Sample()
+  let smcSample nSamples nParticles (dist:Dist<_>) =
+      let samples = dist.SmcMultiple(nSamples, nParticles).Sample()
          
-    [|for sample in samples -> sample.Item , sample.Prob.Value|] 
-          |> Array.groupBy fst 
-          |> Array.map (fun (x,xs) -> x, Array.sumBy snd xs)            
+      [|for sample in samples -> sample.Item , sample.Prob.Value|] 
+            |> Array.groupBy fst 
+            |> Array.map (fun (x,xs) -> x, Array.sumBy snd xs)            
             
 
-let smcSamples nIters nSamples nParticles (dist:Dist<_>) =
-    let updaterate = max 1 (nIters / 10)
-    let mutable nelements = 0
+  let smcSamples nIters nSamples nParticles (dist:Dist<_>) =
+      let updaterate = max 1 (nIters / 10)
+      let mutable nelements = 0
 
-    [|for i in 0..nIters do                                    
-          let samples = smcSample nSamples nParticles dist |> Array.filter (snd >> (<) 1e-12)     
-          nelements <- nelements + samples.Length
+      [|for i in 0..nIters do                                    
+            let samples = smcSample nSamples nParticles dist |> Array.filter (snd >> (<) 1e-12)     
+            nelements <- nelements + samples.Length
         
-          if i % updaterate = 0 then 
-              printfn "%d of %d" i nIters
-              printfn "%d elements" nelements       
-          yield (Map samples)|]
+            if i % updaterate = 0 then 
+                printfn "%d of %d" i nIters
+                printfn "%d elements" nelements       
+            yield (Map samples)|]
 
-    |> Array.fold (fun fm m -> Map.merge (fun p1 p2 -> (p1 + p2)) id m fm) Map.empty 
-    |> Map.toArray 
+      |> Array.fold (fun fm m -> Map.merge (fun p1 p2 -> (p1 + p2)) id m fm) Map.empty 
+      |> Map.toArray 
 
 //////////////////////////////////
 
+//////////////////////////
+module ProbTools =    
+  let filterWith f data = 
+    let matches = data |> Array.filter f
+    (Array.length matches |> float) / (float data.Length) 
+                       
 
-let inline betaMean (a,b) = a / (a + b)  
+  let filterWithCondition conditional f data = 
+    let sub = Array.filter conditional data
+    let matches = sub |> Array.filter f 
+    (Array.length matches |> float) / (float sub.Length) 
 
-let inline dirichletMean (a:Map<_,_>) = let total = Map.sum a in Map.map (fun _ a_i -> a_i / total) a
+
+  let inline probabilityOf filter m = 
+      Array.sumBy snd (Array.filter (fun (k, _) -> filter k) m)
+
+
+  let inline conditionalProbability conditional matchwith m = 
+      let sub = Array.filter (fun (k, _) -> conditional k) m
+      let matches = Array.filter (fun (k, _) -> matchwith k) sub
+      (Array.sumBy snd matches) / (Array.sumBy snd sub)
+
+  let toBits x = x / log 2. 
+
+  let inline log0 x = if x = 0. then 0. else log x
+
+  let inline entropy dist = -Seq.sumBy (fun (_,p) -> p * log0 p) dist
+
+  let inline betaMean (a,b) = a / (a + b)  
+
+  let inline dirichletMean (a:Map<_,_>) = let total = Map.sum a in Map.map (fun _ a_i -> a_i / total) a
  
-let updateBeta (a,b) t =  if t then (a + 1., b) else (a, b + 1.)
+  let updateBeta (a,b) t =  if t then (a + 1., b) else (a, b + 1.)
 
-let updateDirichlet (m:Map<_,_>) x = Map.add x (m.[x] + 1.) m
+  let updateDirichlet (m:Map<_,_>) x = Map.add x (m.[x] + 1.) m
